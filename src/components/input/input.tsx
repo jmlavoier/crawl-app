367be741926dcdb3a2f3e5b30bd7696c07f67121
João Lavoier <jmlavoier@hotmail.com>
2021-01-26 15:49:28 -0300

    feat(components): create input component with tests

---
+++ src/components/input/input.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import {
+  Wrapper,
+  Input as StyledInput,
+  SearchIcon,
+} from './styles';
+
+interface Props {
+  value?: string,
+  onChange?: (e?: React.FormEvent<HTMLInputElement>) => void,
+}
+
+const { useCallback, useRef } = React;
+
+function Input({
+  value, onChange,
+}: Props): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = useCallback(() => {
+    const el = inputRef.current;
+    if (el) {
+      el.focus();
+    }
+  }, []);
+
+  return (
+    <Wrapper>
+      <SearchIcon data-testid="icon" width="22px" height="22px" onClick={handleClick} />
+      <StyledInput ref={inputRef} data-testid="input" value={value} onChange={onChange} />
+    </Wrapper>
+  );
+}
+
+Input.defaultProps = {
+  value: '',
+  onChange: () => {},
+};
+
+export default Input;
---
+++ src/components/input/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { ReactComponent as Search } from 'assets/icons/search.svg';
+
+export const Input = styled.input`
+  font-size: 24px;
+  flex-grow: 1;
+  border: 1;
+  border: 3px solid #000;
+  border-radius: 25px;
+  padding: 5px 40px;
+  outline: none;
+  
+  &:focus {
+    border-color: #ff5824;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 46px;
+  position: relative;
+`;
+
+export const SearchIcon = styled(Search)`
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  cursor: pointer;
+`;
